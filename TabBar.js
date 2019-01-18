b4a96173686fd3e9159bbe6bda10e8a93a72acdf
jaydeep.galani <jaydeep.galani@srkaycg.com>
2019-01-18 14:36:50 +0530

    Fixed issue for not allowing Animation over tabbar.

--- TabBar.js
+++ TabBar.js
@@ -1,25 +1,23 @@
-'use strict';
+"use strict";
 
-import React from 'react';
-import {
-  Animated,
-  Platform,
-  StyleSheet,
-  View,
-} from 'react-native';
+import React from "react";
+import { Animated, Platform, StyleSheet, View } from "react-native";
 
-import ViewPropTypes from './config/ViewPropTypes';
-import Layout from './Layout';
+import ViewPropTypes, { AnimatedViewPropTypes } from "./config/ViewPropTypes";
+import Layout from "./Layout";
 
 export default class TabBar extends React.Component {
   static propTypes = {
-    ...Animated.View.propTypes,
-    shadowStyle: ViewPropTypes.style,
+    ...AnimatedViewPropTypes,
+    shadowStyle: ViewPropTypes.style
   };
 
   render() {
     return (
-      <Animated.View {...this.props} style={[styles.container, this.props.style]}>
+      <Animated.View
+        {...this.props}
+        style={[styles.container, this.props.style]}
+      >
         {this.props.children}
         <View style={[styles.shadow, this.props.shadowStyle]} />
       </Animated.View>
@@ -29,21 +27,21 @@ export default class TabBar extends React.Component {
 
 let styles = StyleSheet.create({
   container: {
-    backgroundColor: '#f8f8f8',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    backgroundColor: "#f8f8f8",
+    flexDirection: "row",
+    justifyContent: "space-around",
     height: Layout.tabBarHeight,
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
-    right: 0,
+    right: 0
   },
   shadow: {
-    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+    backgroundColor: "rgba(0, 0, 0, 0.25)",
     height: Layout.pixel,
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
-    top: Platform.OS === 'android' ? 0 : -Layout.pixel,
-  },
+    top: Platform.OS === "android" ? 0 : -Layout.pixel
+  }
 });
--- config/ViewPropTypes.js
+++ config/ViewPropTypes.js
@@ -1,5 +1,8 @@
-import { View, ViewPropTypes as RNViewPropTypes } from 'react-native';
+
+import { View, ViewPropTypes as RNViewPropTypes ,Animated} from "react-native";
 
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
+
+export const  AnimatedViewPropTypes = Animated.View.propTypes;
  
 export default ViewPropTypes;
